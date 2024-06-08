2a12326870630eab7970c96944fc6dbbc75f35da
Anand M <139520270+Anand07392@users.noreply.github.com>
2024-06-08 20:02:04 +0530

    Create Ghost Gobble Arcade Game.py

---
+++ Ghost Gobble Arcade Game.py
@@ -0,0 +1,28 @@
+def eat_ghost(power_pellet_active, touching_ghost):      
+    if power_pellet_active==True and touching_ghost==True:
+        return True
+    else:
+        return False
+
+
+def score(touching_power_pellet, touching_dot):
+    if touching_dot==True or touching_power_pellet==True:
+        return True
+    else:
+        return False
+
+
+def lose(power_pellet_active, touching_ghost):
+    if power_pellet_active==True or touching_ghost==False:
+        return False
+    else:
+        return True
+
+
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    if has_eaten_all_dots:
+        if touching_ghost and not power_pellet_active:
+            return False  # Pac-Man loses
+        return True  # Pac-Man wins
+    return False  # Pac-Man has not eaten all dots, hence cannot win
+    
