63cdd6112fc2a4f953ee4ab16ce406554e2881f1
Anand M <139520270+Anand07392@users.noreply.github.com>
2024-06-08 22:14:38 +0530

    26

--- Darts.py
+++ Darts.py
@@ -1,11 +1,11 @@
 import math
 def score(x, y):
-    distance = math.sqrt(x**2 + y**2)
-    if distance > 10:
+    dist = math.sqrt(x**2 + y**2)
+    if dist > 10:
         return 0
-    elif distance > 5:
+    elif dist > 5:
         return 1
-    elif distance > 1:
+    elif dist > 1:
         return 5
     else:
         return 10
