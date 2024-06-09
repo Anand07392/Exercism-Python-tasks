7c5da8f3ae86a9585b16246440ee7280bdcfa8a0
Anand M <139520270+Anand07392@users.noreply.github.com>
2024-06-09 13:09:07 +0530

    31

---
+++ ISBN Verifier.py
@@ -0,0 +1,14 @@
+def is_valid(isbn):
+    isbn = isbn.replace('-', '')
+    if len(isbn) != 10:
+        return False
+    if not (isbn[:9].isdigit() and (isbn[9].isdigit() or isbn[9] == 'X')):
+        return False
+    total = 0
+    for i in range(9):
+        total += int(isbn[i]) * (10 - i)
+    if isbn[9] == 'X':
+        total += 10
+    else:
+        total += int(isbn[9])
+    return total % 11 == 0
