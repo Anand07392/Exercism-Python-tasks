8b9e97f32cab5d3c568b3e604d1429ee1e48f05c
Anand M <139520270+Anand07392@users.noreply.github.com>
2024-06-09 18:33:11 +0530

    45

---
+++ Difference of Squares.py
@@ -0,0 +1,11 @@
+def square_of_sum(number):
+    sum_of_numbers = sum(range(1, number + 1))
+    sum_of_numbers=sum_of_numbers**2
+    return sum_of_numbers  
+
+def sum_of_squares(number):
+    sum_of_squares = sum(i ** 2 for i in range(1, number + 1))
+    return sum_of_squares 
+
+def difference_of_squares(number):
+     return square_of_sum(number) - sum_of_squares(number)
