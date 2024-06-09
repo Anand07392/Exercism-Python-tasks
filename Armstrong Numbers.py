0bc850f41e3f35d6935c3359ddbd76f32194572e
Anand M <139520270+Anand07392@users.noreply.github.com>
2024-06-09 09:01:20 +0530

    22

--- Armstrong Numbers.py
+++ Armstrong Numbers.py
@@ -1,5 +1,5 @@
 def is_armstrong_number(number):
     num_str = str(number)
     num_digits = len(num_str)
-    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
-    return armstrong_sum == number
+    sum = sum(int(digit) ** num_digits for digit in num_str)
+    return sum == number
